0f6093741bc3bdfc079e873e69ab6aef05885241
cabralotavio3 <bugvug250@gmail.com>
2024-11-01 10:37:16 -0300

    incompleto pedidos

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-#flask db init
-#flask db migrate -m "Migracao inicial"
-#flask db upgrade
 from flask import Flask
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '123'
-
-#drive://usuario:senha@servidor/banco_dados
-conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/bim3g1"
+app.config['SECRET_KEY'] = 'fdt435t4654756h3q3464756y'
+conexao = 'mysql+pymysql://alunos:cefetmg@127.0.0.1/bim3g1'
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 from database import db
 from flask_migrate import Migrate
-
 db.init_app(app)
 migrate = Migrate(app, db)
-
-from models import Usuario, Cha, Pedido
+from models import Usuario, Pizza, Pedido
+from modulos.usuarios.usuarios import bp_usuario
+from modulos.pizza.pizza import bp_pizza
+app.register_blueprint(bp_usuario, url_prefix='/usuarios')
+app.register_blueprint(bp_pizza, url_prefix='/pizza')
